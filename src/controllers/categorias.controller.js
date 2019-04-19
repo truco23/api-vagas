1fef7b7982d320040a3509efed8cdafef03122ec
Truco23 <trucopaulista23@gmail.com>
2019-04-19 14:38:08 -0300

    Criando o controller para categorias

---
+++ src/controllers/categorias.controller.js
@@ -0,0 +1,17 @@
+const categoriasModel   = require('../models/categorias.model');
+let api                     = {};
+
+api.list = async (req, res) => {
+
+    const categorias = await categoriasModel.find({});
+
+    try {
+        console.log('############# Categorias listadas ###############');
+        res.json(categorias)
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({ fail: error.message })
+    }
+}
+
+module.exports = api;
